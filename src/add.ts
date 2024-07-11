e80db7a8982671246b2147e87584a26adf608ab0
NadiaMonzon <nadiamonque99@gmail.com>
2024-07-11 17:40:30 +0100

    refactor: delete unnnecesary conditional

--- src/add.ts
+++ src/add.ts
@@ -1,12 +1,7 @@
 export function add(numbers: string): number {
-
-  if (numbers.length > 1) {
     const numberList = numbers.split(",");
 
     return numberList.reduce((amount, number) => 
       amount + Number(number), 0
     )
-  }
-
-  return Number(numbers);
 }
