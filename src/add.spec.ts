65b0bcb4766a0ba814ee8cf98058f5b4d003fd66
Daniel Ramos <danielramosacosta1@gmail.com>
2024-07-11 19:32:47 +0200

    refactor: nest in interation 1

--- src/add.spec.ts
+++ src/add.spec.ts
@@ -2,27 +2,29 @@ import { describe, expect, it } from "vitest";
 import { add } from "./add.js";
 
 describe("add", () => {
-  it("returns zero when empty string", () => {
-    const numbers = ""
+  describe("iteration 1", () => {
+    it("returns zero when empty string", () => {
+      const numbers = ""
 
-    const result = add(numbers)
+      const result = add(numbers)
 
-    expect(result).toBe(0)
-  })
+      expect(result).toBe(0)
+    })
 
-  it("returns the number when one number", () => {
-    const numbers = "1"
+    it("returns the number when one number", () => {
+      const numbers = "1"
 
-    const result = add(numbers)
+      const result = add(numbers)
 
-    expect(result).toBe(1)
-  })
+      expect(result).toBe(1)
+    })
 
-  it("returns the sum of two numbers", () => {
-    const numbers = "1,2"
+    it("returns the sum of two numbers", () => {
+      const numbers = "1,2"
 
-    const result = add(numbers)
+      const result = add(numbers)
 
-    expect(result).toBe(3)
-  })
+      expect(result).toBe(3)
+    })
+  });
 })
