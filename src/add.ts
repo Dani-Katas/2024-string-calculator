5df578ece345f52e15d689e31f853dc3fa04867a
NadiaMonzon <nadiamonque99@gmail.com>
2024-07-11 17:57:34 +0100

    green: support different delimiters

--- src/add.ts
+++ src/add.ts
@@ -1,6 +1,15 @@
 const sum = (a: number, b: number) => a + b;
 
 export function add(numbers: string): number {
+
+  if(numbers.startsWith('//')){
+    const alternateNumberList = numbers.substring(4)
+    const finalAlternateNumberList = alternateNumberList.split(";");
+    return  finalAlternateNumberList
+    .map(Number)
+    .reduce(sum, 0);
+  }
+
   const numberList = numbers
     .replaceAll("\n", ",")
     .split(",");
