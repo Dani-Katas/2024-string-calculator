f6c0cf022a1f6c1a8aa3ea3244eb804061649386
Daniel Ramos <danielramosacosta1@gmail.com>
2024-07-11 19:31:36 +0200

    green: returns the sum of two numbers

--- src/add.ts
+++ src/add.ts
@@ -1,3 +1,10 @@
 export function add(numbers: string) {
+
+  if (numbers.length > 1) {
+    const [a, b] = numbers.split(",");
+
+    return Number(a) + Number(b);
+  }
+
   return Number(numbers);
 }
