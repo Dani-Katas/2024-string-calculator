6d6ed69c64abd670e456704034a03fec65771f87
NadiaMonzon <nadiamonque99@gmail.com>
2024-07-11 18:00:06 +0100

    format: add.ts

--- src/add.ts
+++ src/add.ts
@@ -1,20 +1,13 @@
-const sum = (a: number, b: number) => a + b;
+const sum = (a: number, b: number) => a + b
 
 export function add(numbers: string): number {
-
-  if(numbers.startsWith('//')){
+  if (numbers.startsWith("//")) {
     const alternateNumberList = numbers.substring(4)
-    const finalAlternateNumberList = alternateNumberList.split(";");
-    return  finalAlternateNumberList
-    .map(Number)
-    .reduce(sum, 0);
+    const finalAlternateNumberList = alternateNumberList.split(";")
+    return finalAlternateNumberList.map(Number).reduce(sum, 0)
   }
 
-  const numberList = numbers
-    .replaceAll("\n", ",")
-    .split(",");
+  const numberList = numbers.replaceAll("\n", ",").split(",")
 
-  return numberList
-    .map(Number)
-    .reduce(sum, 0);
+  return numberList.map(Number).reduce(sum, 0)
 }
