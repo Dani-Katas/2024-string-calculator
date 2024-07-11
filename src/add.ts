dc6e08e4ef4e4d1a8b2793911d7ef635fb3fbed2
Daniel Ramos <danielramosacosta1@gmail.com>
2024-07-11 20:09:07 +0200

    green: supports other delimiters

--- src/add.ts
+++ src/add.ts
@@ -3,7 +3,8 @@ const sum = (a: number, b: number) => a + b
 export function add(numbers: string, delimiter = ","): number {
   if (numbers.startsWith("//")) {
     const alternateNumberList = numbers.substring(4)
-    return add(alternateNumberList, ";")
+    const delimiter = numbers.substring(2, 3)
+    return add(alternateNumberList, delimiter)
   }
 
   const numberList = numbers.replaceAll("\n", delimiter).split(delimiter)
